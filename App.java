0c48966aa58428dfd8f8fd179f64f461e171ebff
Taylor Holberton <taylorcholberton@gmail.com>
2020-02-29 19:32:40 -0500

    Referencing window class in App

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 /** This is the top level class for the project.
  * For right now, since there's only one tile generator,
  * it just goes straight into the wall generator window.
+ * @see WallGeneratorView WallGeneratorView
  * */
 public class App {
   public static void main(String[] args) {
