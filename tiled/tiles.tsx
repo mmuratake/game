6c4a43db951c5f5fd804b2fe94f71e0ec858dece
Taylor Holberton <taylorcholberton@gmail.com>
2020-03-03 17:46:50 -0500

    Adding textures/tileset/tilemap

---
+++ tiled/tiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="house_interior" tilewidth="100" tileheight="100" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="../textures/house_interior/corner.png"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="../textures/house_interior/floor.png"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="100" source="../textures/house_interior/top_corner.png"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="../textures/house_interior/top_wall.png"/>
+ </tile>
+ <tile id="4">
+  <image width="100" height="100" source="../textures/house_interior/wall.png"/>
+ </tile>
+</tileset>
