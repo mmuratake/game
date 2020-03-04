545000e36d6cfe9e9c22ebe0e1b489d5b3dff65d
Taylor Holberton <taylorcholberton@gmail.com>
2020-03-03 22:21:13 -0500

    Adding textures, tilesets, and tilemaps

--- tiled/tiles.tsx
+++ tiled/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="house_interior" tilewidth="100" tileheight="100" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.3.1" name="house_interior" tilewidth="400" tileheight="400" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="100" height="100" source="../textures/house_interior/corner.png"/>
@@ -15,5 +15,35 @@
  </tile>
  <tile id="4">
   <image width="100" height="100" source="../textures/house_interior/wall.png"/>
+ </tile>
+ <tile id="5">
+  <image width="100" height="100" source="../textures/outdoors/grass.png"/>
+ </tile>
+ <tile id="8">
+  <image width="100" height="100" source="../textures/templates/path_outline.png"/>
+ </tile>
+ <tile id="9">
+  <image width="100" height="100" source="../textures/outdoors/dirt_path_t_section.png"/>
+ </tile>
+ <tile id="10">
+  <image width="100" height="100" source="../textures/outdoors/dirt_path_corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="100" height="100" source="../textures/outdoors/dirt_path_cross_section.png"/>
+ </tile>
+ <tile id="12">
+  <image width="100" height="100" source="../textures/outdoors/dirt_path_linear.png"/>
+ </tile>
+ <tile id="13">
+  <image width="100" height="100" source="../textures/outdoors/dirt_path_dead_end.png"/>
+ </tile>
+ <tile id="14">
+  <image width="100" height="100" source="../textures/templates/todo.png"/>
+ </tile>
+ <tile id="16">
+  <image width="400" height="400" source="../textures/characters/fairy.gif"/>
+ </tile>
+ <tile id="17">
+  <image width="100" height="100" source="../textures/house_interior/bed.png"/>
  </tile>
 </tileset>
